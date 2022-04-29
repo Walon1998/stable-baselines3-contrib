1bf4cd0cd5c215d6fba623b57672af4e24ba83d1
Neville Walo <neville.walo@gmail.com>
2022-04-29 21:20:58 +0200

    improved torch device to

--- sb3_contrib/common/recurrent/policies.py
+++ sb3_contrib/common/recurrent/policies.py
@@ -423,8 +423,8 @@ class RecurrentActorCriticPolicy(ActorCriticPolicy):
 
         with th.no_grad():
             # Convert to PyTorch tensors
-            states = th.tensor(state[0]).float().to(self.device), th.tensor(state[1]).float().to(self.device)
-            episode_starts = th.tensor(episode_start).float().to(self.device)
+            states = th.tensor(state[0], dtype=torch.float32, device=self.device), th.tensor(state[1], dtype=torch.float32, device=self.device)
+            episode_starts = th.tensor(episode_start, dtype=torch.float32, device=self.device)
             actions, states = self._predict(
                 observation, lstm_states=states, episode_starts=episode_starts, deterministic=deterministic
             )
